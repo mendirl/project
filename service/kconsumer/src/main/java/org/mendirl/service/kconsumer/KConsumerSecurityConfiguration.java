4df5017ca869a5013a12767fdd218b13e60ac252
Fabien Couillard <couillard.fabien+dev@gmail.com>
2018-07-13 12:51:07 +0200

    add applications kconsumer and kproducer

---
+++ service/kconsumer/src/main/java/org/mendirl/service/kconsumer/KConsumerSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package org.mendirl.service.kconsumer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class KConsumerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+            .and().csrf().disable();
+    }
+}
