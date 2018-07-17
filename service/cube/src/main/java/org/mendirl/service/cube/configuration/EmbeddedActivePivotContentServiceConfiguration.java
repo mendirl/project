793071b2f4d1053851c7a704f3e1abf9d4a9f1f1
Fabien Couillard <couillard.fabien+dev@gmail.com>
2018-07-17 15:28:59 +0200

    add an embedded db (H2) for the content server (embedded and remote)

--- service/cube/src/main/java/org/mendirl/service/cube/configuration/EmbeddedActivePivotContentServiceConfiguration.java
+++ service/cube/src/main/java/org/mendirl/service/cube/configuration/EmbeddedActivePivotContentServiceConfiguration.java
@@ -1,19 +1,31 @@
 package org.mendirl.service.cube.configuration;
 
 import com.qfs.content.service.IContentService;
-import com.qfs.content.service.impl.InMemoryContentService;
+import com.qfs.content.service.audit.impl.AuditableHibernateContentService;
 import com.qfs.pivot.content.IActivePivotContentService;
 import com.qfs.pivot.content.impl.ActivePivotContentServiceBuilder;
 import com.qfs.server.cfg.content.IActivePivotContentServiceConfig;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.dialect.H2Dialect;
+import org.hibernate.tool.schema.Action;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 import static org.mendirl.service.cube.CubeSecurityConfiguration.ROLE_USER;
 
 @Configuration
 @Profile({"embedded-content"})
 public class EmbeddedActivePivotContentServiceConfiguration implements IActivePivotContentServiceConfig {
+
+    private DataSource dataSource;
+
+    public EmbeddedActivePivotContentServiceConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     @Override
@@ -25,6 +37,16 @@ public class EmbeddedActivePivotContentServiceConfiguration implements IActivePi
     @Bean
     @Override
     public IContentService contentService() {
-        return new InMemoryContentService();
+        Properties properties = new Properties();
+
+        properties.put(AvailableSettings.DIALECT, H2Dialect.class.getCanonicalName());
+        properties.put(AvailableSettings.DATASOURCE, dataSource);
+        properties.put(AvailableSettings.HBM2DDL_AUTO, Action.UPDATE);
+
+        org.hibernate.cfg.Configuration conf = new org.hibernate.cfg.Configuration();
+        conf.addProperties(properties);
+
+        return new AuditableHibernateContentService(conf);
     }
+
 }
