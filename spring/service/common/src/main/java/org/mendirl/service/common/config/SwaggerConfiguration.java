2dcb604b1f06a11301ee81450fecbc236831328a
Fabien Couillard <couillard.fabien+dev@gmail.com>
2020-04-22 09:21:32 +0200

    migrate to webflux

--- spring/service/common/src/main/java/org/mendirl/service/common/config/SwaggerConfiguration.java
+++ spring/service/common/src/main/java/org/mendirl/service/common/config/SwaggerConfiguration.java
@@ -2,22 +2,15 @@ package org.mendirl.service.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/swagger", "/swagger-ui.html");
-    }
+@EnableSwagger2WebFlux
+public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
--- spring/service/console/src/main/java/org/mendirl/service/console/ConsoleSecurityConfiguration.java
+++ spring/service/console/src/main/java/org/mendirl/service/console/ConsoleSecurityConfiguration.java
@@ -1,15 +1,18 @@
 package org.mendirl.service.console;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-public class ConsoleSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class ConsoleSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()//
-            .and().csrf().disable();
+    @Bean
+    public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+            .anyExchange().permitAll()
+            .and().build();
     }
+
 }
