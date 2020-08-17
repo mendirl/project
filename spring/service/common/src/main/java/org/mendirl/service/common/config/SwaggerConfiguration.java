fe8321a8847b89e77f5ad14e18fe3c1005044edb
Fabien Couillard <couillard.fabien+dev@gmail.com>
2020-08-17 21:56:27 +0200

    clean springfox-swagger code with v3

--- spring/service/common/src/main/java/org/mendirl/service/common/config/SwaggerConfiguration.java
+++ spring/service/common/src/main/java/org/mendirl/service/common/config/SwaggerConfiguration.java
@@ -1,23 +1,24 @@
 package org.mendirl.service.common.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2WebFlux
+//@Configuration
+//@EnableSwagger2
 public class SwaggerConfiguration {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build();
-    }
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//            .select()
+//            .apis(RequestHandlerSelectors.any())
+//            .paths(PathSelectors.any())
+//            .build();
+//    }
 }
