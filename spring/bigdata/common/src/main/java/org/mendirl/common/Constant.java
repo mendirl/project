7412f56152e612c9d9125134a01083c9a3069b1d
Fabien Couillard <couillard.fabien+dev@gmail.com>
2020-12-27 17:57:44 +0100

    change folders

--- spring/bigdata/common/src/main/java/org/mendirl/common/Constant.java
+++
@@ -1,8 +0,0 @@
-package org.mendirl.common;
-
-public class Constant {
-    public static final String PATH_TARIFS_CSV = "data/csv/tarifs.csv";
-
-    public static final String PATH_TARIFS_PARQUET_OUTPUT = "data/parquet/tarifs.parquet";
-
-}
